2c7e7c43b7bf6f7f53d5a0b2aad8ca2f1b2278ac
samuraikun <tazitaziawawa@gmail.com>
2018-12-28 09:52:54 +0900

    turn on HMR

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.getElementById('root') as HTMLElement;
+
+const render = () => {
+  ReactDOM.render(
+    <App />,
+    rootElement
+  );
+}
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    setTimeout(render);
+  });
+}
+
+render();
+
 registerServiceWorker();
